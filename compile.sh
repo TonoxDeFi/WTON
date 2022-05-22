0a4899c2a4ecc1c429c2a86b1c0a889241319ba4
dariotarantini <dariotarantinimail@gmail.com>
2022-05-22 18:28:16 +0200

    updated contracts

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
 echo -e "\e[32mBuilding...\e[0m"
-func -SPA -o ./build/jetton-wallet.fif ./stdlib.fc ./src/math.func ./src/utils.func ./src/params.fc ./src/op-codes.fc ./src/jetton-utils.fc ./src/jetton-wallet.fc
-func -SPA -o ./build/jetton-minter.fif ./stdlib.fc ./src/math.func ./src/utils.func ./src/params.fc ./src/op-codes.fc ./src/jetton-utils.fc ./src/jetton-minter.fc
+func -SPA -o ./build/minter.fif ./src/minter.func
+func -SPA -o ./build/wallet.fif ./src/wallet.func
 
-echo "jetton-wallet: " && echo '"build/jetton-wallet.fif" include 2 boc+>B "build/jetton-wallet.boc" B>file' | fift -s
-echo "jetton-minter: " && echo '"build/jetton-minter.fif" include 2 boc+>B "build/jetton-minter.boc" B>file' | fift -s
+echo "minter: " && echo '"build/minter.fif" include 2 boc+>B "build/minter.boc" B>file' | fift -s
+echo "wallet: " && echo '"build/wallet.fif" include 2 boc+>B "build/wallet.boc" B>file' | fift -s
 echo -e "\n\e[31mHex:\e[0m"
 
 fift -s build/print-hex.fif
